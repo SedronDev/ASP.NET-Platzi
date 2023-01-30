c7ebebc9d81d6df3f43b9b3a7545ae09ad280076
sedron <jfabianabadd@gmail.com>
2023-01-30 18:13:32 -0500

    Types stronged and changed default index

--- Models/Escuela.cs
+++ Models/Escuela.cs
@@ -2,7 +2,7 @@ namespace ASP_Platzi.Models;
 
 public class Escuela
 {
-    public string EscuelaId {get;set;}
-    public string Nombre {get;set;}
+    public string? EscuelaId {get;set;}
+    public string? Nombre {get;set;}
     public int AñoFundacion {get;set;}
 }
