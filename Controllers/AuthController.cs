277d8abbfacafd10f000ee0ef57b8b9d1c764a8c
sedron <jfabianabadd@gmail.com>
2023-02-15 10:42:55 -0500

    Implemented JWT

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using ASP_Platzi.Models;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
 namespace ASP_Platzi.Controllers;
 
 public class AuthController: Controller
 {
     public static User user = new User();
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
     [HttpPost]
     public IActionResult Register(UserDto request)
@@ -29,6 +40,31 @@ public class AuthController: Controller
             return BadRequest("Wrong password");
         }
 
-        return Ok(user);
+        string Token = CreateToken(user);
+
+        return Ok(Token);
+    }
+
+    private string CreateToken(User user)
+    {
+        List<Claim> claims = new List<Claim> {
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            _configuration.GetSection("AppSettings:Token").Value!
+        ));
+
+        var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: cred    
+        );
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
     }
 }
